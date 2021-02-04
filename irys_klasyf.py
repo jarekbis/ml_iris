ef85c4466d9404fb7b660b8e178e4a7c0aa2e3e0
jarekbis <32929780+jarekbis@users.noreply.github.com>
2021-02-04 13:28:46 +0100

    Dodany import numpy

--- irys_klasyf.py
+++ irys_klasyf.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
